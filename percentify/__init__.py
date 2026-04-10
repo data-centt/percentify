cc7c640579733246e40fb5c2deba43dd1ac40a82
Daniel15568 <1danielemakporuena@gmail.com>
2026-04-10 11:32:56 +0100

    bumped version to 0.2.1

--- percentify/__init__.py
+++ percentify/__init__.py
@@ -1,4 +1,4 @@
 from .core import percent, percent_change, percent_diff, percent_distribute, percent_format
 
 __all__ = ["percent", "percent_change", "percent_diff", "percent_distribute", "percent_format"]
-__version__ = "0.2.0"
+__version__ = "0.2.1"
