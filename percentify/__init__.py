a26520718c3a3a7c46fcfb3252298f4a765dff25
Daniel15568 <1danielemakporuena@gmail.com>
2026-06-30 17:02:12 +0100

    version bumped

--- percentify/__init__.py
+++ percentify/__init__.py
@@ -2,4 +2,4 @@ from .core import percent, change, difference, split, display
 from .stats import vif, missing, cv, outliers, r_squared, variance_explained
 
 __all__ = ["percent", "change", "difference", "split", "display", "vif", "missing", "cv", "outliers", "r_squared", "variance_explained"]
-__version__ = "0.3.2"
+__version__ = "0.4.0"
