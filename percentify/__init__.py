493a1d90d5f853dc28ce509604de17f832163510
Daniel15568 <1danielemakporuena@gmail.com>
2026-07-07 00:59:41 +0100

    major changes, version bumped to 1.0.0

--- percentify/__init__.py
+++ percentify/__init__.py
@@ -1,4 +1,4 @@
 from .stats import change, vif, missing, cv, outliers, r_squared, pca_variance, PercentifyWarning
 
 __all__ = ["change", "vif", "missing", "cv", "outliers", "r_squared", "pca_variance", "PercentifyWarning"]
-__version__ = "0.5.0"
+__version__ = "1.0.0"
